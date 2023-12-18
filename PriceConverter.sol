6a8d39aba3bf04eeb0b1986b51aa6ab58a32844e
StackWeaver <stackweaver7@gmail.com>
2023-12-18 21:25:41 +0100

    Create PriceConverter.sol
    The PriceConverter library interacts with a Chainlink Price Feed to obtain the latest price information and provides functions to convert Ether amounts to their equivalent values in USD.
    
    Import Statement:
    'import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";': Imports the Chainlink 'AggregatorV3Interface' to interact with the Price Feed.
    
    Library Definition - PriceConverter:
    Defines a library named 'PriceConverter'.
    
    Internal Function - getPrice:
    'function getPrice() internal view returns (uint256) { ... }': Retrieves the latest price from the Chainlink Price Feed and returns it as a 'uint256' value (multiplied by 1e10 for precision).
    
    Internal Function - getVersion:
    'function getVersion() internal view returns (uint256) { ... }': Gets the version of the Chainlink Price Feed and returns it.
    
    Internal Function - getConversionRate:
    'function getConversionRate(uint256 ethAmount) internal view returns (uint256) { ... }': Calculates the equivalent USD value of a given Ether amount using the current Ether price from the Price Feed. Returns the calculated USD value.

---
+++ PriceConverter.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() internal  view returns (uint256) {
+        // ABI needed
+        // Address needed 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+        (,int price,,,) = priceFeed.latestRoundData();
+
+        return uint256(price * 1e10);
+    }
+
+    function getVersion() internal  view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) internal  view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
